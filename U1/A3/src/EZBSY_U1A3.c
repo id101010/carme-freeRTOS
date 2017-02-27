c1932e89146ca2373cf814612e760c7cdd73c6a3
aschmocker <schmocker@puzzle.ch>
2017-02-27 13:34:11 +0100

    Implemented potentiometer controllable moving light

--- U1/A3/src/EZBSY_U1A3.c
+++ U1/A3/src/EZBSY_U1A3.c
@@ -25,7 +25,7 @@
 
 //----- Header-Files -----------------------------------------------------------
 #include <carme.h>                      /* CARME Module                       */
-//#include <carme_io1.h>                  /* CARMEIO1 Board Support Package     */
+//#include <carme_io1.h>                /* CARMEIO1 Board Support Package     */
 #include <carme_io2.h>
 
 #include <stdio.h>                      /* Standard Input/Output              */
@@ -47,34 +47,21 @@
 #define PRIORITY_TASK2         ( 1 )  // TODO: modify priority, low priority number
                                       // denotes low priority task
 
-//#define USE_vWAIT               // TODO: set this compiler switch to use CPU-wait
-                                // instead of vTastDelay
-
-//ADC stuff
-#define SQR3_SQ_SET            ((uint32_t)0x0000001F)  
-#define SMPR1_SMP_SET          ((uint32_t)0x00000007)
-//Mask for the ADC value (to cut away bits which cannot be valid)
-#define ADC_MASK 0x3FF 
-//Value that should be threated as maximum
-#define ADC_MAX 0x3A0
-//Tolerance value. Changes below this value will be ignored.
-#define ADC_TOLERANCE 0x08
+//#define USE_vWAIT                   // TODO: set this compiler switch to use CPU-wait
+                                      // instead of vTastDelay
 
 //----- Data types -------------------------------------------------------------
 
 //----- Function prototypes ----------------------------------------------------
 static void  vAppTask1(void *pvData);
 static void  vAppTask2(void *pvData);
-static void  adc_init(void);
-static uint16_t adc_read(void);
 
 #ifdef USE_vWAIT
 static void  vWait(void);
 #endif
 
 //----- Data -------------------------------------------------------------------
-uint16_t u16Speed;
-volatile unsigned char* LED = (volatile unsigned char*)0x6C000200;
+uint16_t u16Speed = 100;
 
 //----- Implementation ---------------------------------------------------------
 
@@ -95,11 +82,9 @@ int  main(void) {
     /* Ensure all priority bits are assigned as preemption priority bits. */
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
 
-    CARME_IO1_Init();               // Initialize the CARMEIO1
-    CARME_IO1_LED_Set(0, 0xFF);     // Reset led state
-
-    /* initialize adc */
-    adc_init();
+    CARME_IO1_Init();
+    CARME_IO2_Init();
+    CARME_IO1_LED_Set(0x01, 0xFF);  // Turn on LED 1 and 8
 
     xTaskCreate(vAppTask1,
                 "Task1",
@@ -120,6 +105,7 @@ int  main(void) {
     /* code never reached */
     for (;;) {
     }
+
     return 0;
 }
 
@@ -137,24 +123,30 @@ int  main(void) {
  ******************************************************************************/
 static void vAppTask1(void *pvData) {
 
-    uint8_t     u8LED1to6;
-    uint8_t     u8Led;
+    uint8_t     u8LED1to8;
+    uint8_t     u8Led = 0;
 
-    while(1) {
+    u16Speed = 100;
 
-    /* Get LED state, shift left, check overflow and show again */
-    CARME_IO1_LED_Get(&u8Led);
-    u8LED1to6 = u8Led & 0xFF;                /* Mask all bits */
-    u8LED1to6 <<= 1;                         /* Shift LED left one position */
-    if (u8LED1to6 == 0x80)   {
-        u8LED1to6 = 1;                       /* LED 7 on?, set LED1 */
-    }
-    CARME_IO1_LED_Set(u8LED1to6, 0xFF);
+    while(1){
 
+        /* Get LED state, shift left, check overflow and show again */
+        CARME_IO1_LED_Get(&u8Led);
+     
+        u8LED1to8 = u8Led & 0b11111111;          /* Mask all bits */
+     
+        if (u8LED1to8 == 0b10000000){
+            u8LED1to8 = 0b00000001;              /* LED 8 on?, set LED1 */
+        }else{
+            u8LED1to8 <<= 1;                     /* Shift LED left one position */
+        }
+     
+        CARME_IO1_LED_Set(u8LED1to8, 0xFF);
+   
 #ifdef USE_vWAIT
         vWait();
 #else
-        vTaskDelay(100);
+        vTaskDelay(u16Speed);
 #endif
     }
 }
@@ -174,15 +166,14 @@ static void vAppTask1(void *pvData) {
 static void  vAppTask2(void *pvData) {
 
     while(1) {
-        u16Speed = adc_read();
+        CARME_IO2_ADC_Get(CARME_IO2_ADC_PORT0, &u16Speed);
 #ifdef USE_vWAIT
         vWait();
 #else
-        vTaskDelay(100);
+        vTaskDelay(u16Speed);
 #endif
     }
 }
-
 
 /*******************************************************************************
  *  function :    wait
@@ -205,67 +196,3 @@ static void vWait(void) {
     }
 }
 #endif
-
-/*******************************************************************************
- *  function :    read_init
- ******************************************************************************/
-/** \brief        initializes the analog to digital converter
- *
- *  \type         local
- *
- *  \return       void
- *
- ******************************************************************************/
-void adc_init() {
-    //Enable the peripheral clock of GPIOB
-    RCC->AHB1ENR |= RCC_AHB1Periph_GPIOB;
-    //Choose the working mode of PB0 with the GPIO port mode register
-    GPIOB->MODER &= ~GPIO_MODER_MODER0;
-    GPIOB->MODER |=  GPIO_Mode_AN;
-    //Configure the GPIO port pull-up/pull-down register for PB0
-    //Page 282/1718 of "RM0090 Reference Reference Manual (October 2014)"
-    GPIOB->PUPDR &= ~GPIO_PUPDR_PUPDR0;
-    GPIOB->PUPDR |=  GPIO_PuPd_UP;
-    //Initialize the ADC
-    //Enable the peripheral clock of the ADC
-    RCC->APB2ENR |= RCC_APB2Periph_ADC;
-    //Configure ADC1: scan conversion mode and resolution
-    //Set SCAN bit according to ADC_ScanConvMode value
-    //Set RES bit according to ADC_Resolution value
-    ADC1->CR1 = ADC_Resolution_10b;
-    // Configure ADC1: regular channel sequence length
-    // Set L bits according to ADC_NbrOfConversion value
-    ADC1->SQR1 = 0;
-    // Set the ADON bit to enable the ADC
-    ADC1->CR2 = ADC_CR2_ADON;
-}
-
-/*******************************************************************************
- *  function :    read_adc
- ******************************************************************************/
-/** \brief        reads the adc to which the potentiometer is connected
- *
- *  \type         local
- *
- *  \return       uint16_t adcvalue
- *
- ******************************************************************************/
-uint16_t adc_read(){
-    uint16_t value = 0;
-    
-    // Specify the sample time for the conversion
-    ADC1->SMPR2 &= SMPR1_SMP_SET           << (3 * ADC_Channel_8);
-    ADC1->SMPR2 |= ADC_SampleTime_15Cycles << (3 * ADC_Channel_8);
-    // Set the channel 8 as the first conversion in the ADC reg seq register
-    ADC1->SQR3  &= ~SQR3_SQ_SET;
-    ADC1->SQR3  |= ADC_Channel_8;
-    // Start the conversion
-    ADC1->CR2 |= ADC_CR2_SWSTART;   
-    // Wait until the conversion has been done
-    while( (ADC1->SR & ADC_FLAG_EOC) == 0 );
-    // Read the value
-    value = ADC1->DR;
-    value &= ADC_MASK;
-    
-    return value;
-}
